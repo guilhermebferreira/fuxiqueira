fecdce3a5f3edaca54a68fbdfa7bfba41bba4178
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-04-16 23:50:25 -0300

    [UPDATE] configurações do projeto e do modulo questões atualizadas

--- api/fuxiqueira/settings.py
+++ api/fuxiqueira/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+UPLOAD_IMAGE_DIR = os.path.join(BASE_DIR, 'upload')
 
 
 # Quick-start development settings - unsuitable for production
--- api/questoes/apps.py
+++ api/questoes/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class QuestoesConfig(AppConfig):
-    name = 'questoes'
+    name = 'questao'
