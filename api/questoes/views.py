412363204399f3f24bf3e79af8465978989aebb4
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-04-16 23:51:42 -0300

    [UPDATE] listando as questões cadastradas no app

--- api/questoes/views.py
+++ api/questoes/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Questao
+from .serializers import QuestaoSerializer
+
+
+class QuestaoViewSet(viewsets.ModelViewSet):
+    serializer_class = QuestaoSerializer
+
+    def get_queryset(self):
+      queryset = Questao.objects.all().order_by('projeto_id', 'id')
+      if len(self.kwargs) and 'id' in self.kwargs:
+          id = self.kwargs['id']
+          queryset = queryset.filter(id=id)
+      return queryset
