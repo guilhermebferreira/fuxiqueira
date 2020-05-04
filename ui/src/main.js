a6bef2f868371d988228add99cf628014fb0af0d
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-05-04 18:27:59 -0300

    [ADD] resstruturando docker-compose e dokerfile da UI

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,11 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import store from './store';
+import vuetify from './plugins/vuetify';
+import '@babel/polyfill';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
-  render: h => h(App)
-}).$mount("#app");
+  vuetify,
+  render: (h) => h(App),
+}).$mount('#app');
